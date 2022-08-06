8454dd5aacd20042ac4e050ab747edd92c8a368a
Brando Rodriguez <brandorodriguez@gmail.com>
2022-08-06 14:59:37 -0500

    New Project

--- components/Landing/Footer/index.js
+++ components/Landing/Footer/index.js
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <footer className={styles.footer}>
             <div>
-                {/* <img src="img/items/logo3.png" /> */}
+                <img src="img/items/logo3.png" />
             </div>
             <div>
                 <ul>
