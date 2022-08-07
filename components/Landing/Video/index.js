ba99d356682d0c8fca588e75c8aeec225c6872de
Brando Rodriguez <brandorodriguez@gmail.com>
2022-08-06 22:39:28 -0500

    New Project

--- components/Landing/Video/index.js
+++ components/Landing/Video/index.js
@@ -4,24 +4,23 @@ function Video() {
     return (
         <header id={styles.presentation} className={styles.video_presentation}>
 
-            <picture>
 
-                <video
-                    autoPlay="autoPlay"
-                    loop="loop"
-                    muted
-                    defaultmuted=""
-                    playsInline=""
-                    preload="auto"
-                    id="miVideo"
-                >
-                    <source media="(max-width: 600px)" src="/presentation1.mp4" />
 
-                    <source src="/presentation2.mp4" type="video/mp4" />
+            <video
+                autoPlay="autoPlay"
+                loop="loop"
+                muted
+                defaultmuted=""
+                playsInline=""
+                preload="auto"
+                id="miVideo"
+            >
+                <source media="(max-width: 600px)" src="/presentation1.mp4" />
 
-                </video>
+                <source src="/presentation2.mp4" type="video/mp4" />
 
-            </picture>
+            </video>
+
         </header>
 
     )
