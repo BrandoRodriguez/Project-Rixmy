8473498bbbeed5ca28a71de8f4a5c8a1acbb36f2
Brando Rodriguez <brandorodriguez@gmail.com>
2022-08-10 19:08:42 -0500

    subo cambios

--- components/Landing/Section3/index.js
+++ components/Landing/Section3/index.js
@@ -7,40 +7,20 @@ function Section3({ data }) {
   const class_name = data.id.replace(/-/g, '_')
   const section_name = class_name + '_section'
 
-  const styles_config = `@media (min-width: 1023px) {
-    .content {
-      width: 70%;
-      padding-left: 80px;
-    }
-  }`
-
-  const variable = 'polygon(0 0, 0 100%, 100% 100%, 100% 7%, 50% 7%, 50% 0);'
-
   return (
 
     <section
       className={`${styles.article} ${styles.bg}`}
     >
-      <div style={{
-        background: `${data.style.position} url(/img/${data.style.background})`,
-        clipPath: `${variable}`,
+      <div className={styles.bg_image} style={{
+        backgroundImage: `url(/img/${data.style.background})`,
         backgroundSize: 'cover',
         width: '100%',
         height: '100vh'
       }}>
 
       </div>
-      {/* <img
-        src="/img/items/forma.png"
-        alt=""
-        style={{
-          position: 'relative',
-          width: '100%',
-          top: '-100px',
 
-        }}
-      /> */}
-      
       {/* <div>
         <div className={styles.content}>
 
