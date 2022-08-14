46cac0a0eb810c897efe01e32346ad36e714eaf0
Brando Rodriguez <brandorodriguez@gmail.com>
2022-08-13 21:06:48 -0500

    .

--- pages/blog/article/[slug].js
+++ pages/blog/article/[slug].js
@@ -1,6 +1,6 @@
 
-import Seo from "../../components/seo"
-import { getStrapiMedia } from "../../lib/media"
+import Seo from "../../../components/seo"
+import { getStrapiMedia } from "../../../lib/media"
 
 function Article({ article, categories }) {
 
@@ -15,41 +15,41 @@ function Article({ article, categories }) {
 
   return (
     <div>
-        <Seo seo={seo} />
-        Aqui va el contenido del articulo
+      <Seo seo={seo} />
+      Aqui va el contenido del articulo
     </div>
   )
 }
 
 export async function getStaticPaths() {
-    const articlesRes = await fetchAPI("/articles", { fields: ["slug"] })
-  
-    return {
-      paths: articlesRes.data.map((article) => ({
-        params: {
-          slug: article.attributes.slug,
-        },
-      })),
-      fallback: false,
-    }
-  }
-  
-  export async function getStaticProps({ params }) {
-    const articlesRes = await fetchAPI("/articles", {
-      filters: {
-        slug: params.slug,
+  const articlesRes = await fetchAPI("/articles", { fields: ["slug"] })
+
+  return {
+    paths: articlesRes.data.map((article) => ({
+      params: {
+        slug: article.attributes.slug,
       },
-      populate: "*",
-    })
-    const categoriesRes = await fetchAPI("/categories")
-  
-    return {
-      props: { 
-        article: articlesRes.data[0],
-        categories: categoriesRes 
-      },
-      revalidate: 1,
-    }
+    })),
+    fallback: false,
   }
+}
+
+export async function getStaticProps({ params }) {
+  const articlesRes = await fetchAPI("/articles", {
+    filters: {
+      slug: params.slug,
+    },
+    populate: "*",
+  })
+  const categoriesRes = await fetchAPI("/categories")
+
+  return {
+    props: {
+      article: articlesRes.data[0],
+      categories: categoriesRes
+    },
+    revalidate: 1,
+  }
+}
 
 export default Article
